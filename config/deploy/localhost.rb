d19db04ab9e17e026fe501d32d0c26890ae9e4fb
Kevin S. Clarke <ksclarke@ksclarke.io>
2018-03-28 17:19:13 -0400

    Change localhost env to 'development'

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -1,4 +1,4 @@
 set :stage, :localhost
-set :rails_env, 'production'
+set :rails_env, 'development'
 set :deploy_to, '/opt/nurax'
 server '127.0.0.1', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
